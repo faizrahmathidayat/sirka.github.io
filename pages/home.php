5eea80d49e485e248bcf919ec0a83dbf35ee144c
faizrahmathidayat <43479864+faizrahmathidayat@users.noreply.github.com>
2019-03-07 20:47:22 +0700

    Create home.php

---
+++ pages/home.php
@@ -0,0 +1,14 @@
+<div class="row">
+<div class="col-md-11">
+  <br>
+<div class="col-md-offset-1 box">
+  <h1><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;HOME</h1>
+  <hr>
+  <hr>
+<h4>Si-RKA adalah Aplikasi Laporan Rencana Kerja dan Anggaran  berbasis web yang membantu membuat suatu laporan rekapitulasi
+Rencana Kerja dan Anggaran pada Unit Pelaksana Teknis Upt Pendidikan Dasar Kecamatan Cibodas.
+
+</h4>
+</div>
+</div>
+</div>
